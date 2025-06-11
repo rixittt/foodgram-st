851744b6b75bc617e448eb3e88c0c9c1dcc6d099
Timur <113575005+rixittt@users.noreply.github.com>
2025-06-11 13:11:40 +0500

    Update index.js

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/rixittt/foodgram-st" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://vk.com/rixit" className={styles.textLink}>Тимур</a>
             </p>
           </div>
         </aside>
